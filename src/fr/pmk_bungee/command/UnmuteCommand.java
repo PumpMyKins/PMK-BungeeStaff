c53fefccf1f0476d56fd60785779357221c90bdd
Sufike <l.christ54260@gmail.com>
2018-10-31 15:25:09 +0100

    unmute without feature oups fail

--- src/fr/pmk_bungee/command/UnmuteCommand.java
+++ src/fr/pmk_bungee/command/UnmuteCommand.java
@@ -1,5 +1,7 @@
 package fr.pmk_bungee.command;
 
+import fr.pmk_bungee.Main;
+import fr.pmk_bungee.utils.PlayerProfile;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -10,10 +12,30 @@ public class UnmuteCommand extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings({ "deprecation", "unused" })
 	@Override
-	public void execute(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public void execute(final CommandSender sender, final String[] args) {
+		  if(sender.hasPermission("bungeeban.command.unmute")) {
+			  if(args.length == 1) {
+				  String playerName = args[0];
+				  PlayerProfile profile = new PlayerProfile(playerName);
+				  if(profile != null) {
+					  if(profile.isMuted()) {
+						  profile.unmute();
+						  sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.commands.mute.muted", new String[] { "{NAME}~" + playerName }));							  
+					  }
+					  else {
+						  sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.errors.player_not_muted", new String[] { "{NAME}~" + playerName }));				  }
+				  }
+				  else {
+					  sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.errors.player_not_found"));			  }
+			  }
+			  else {
+				  sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.commands.unmute.syntax"));
+			  }
+		  }
+		  else {
+			  sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.errors.no_permissions"));	  }
+	  }
 
 }
