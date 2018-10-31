febe5c8a3f47bcefa88a878c5a69a38cafb1e6c0
Sufike <l.christ54260@gmail.com>
2018-10-31 17:21:46 +0100

    init history

--- src/fr/pmk_bungee/command/HistoryCommand.java
+++ src/fr/pmk_bungee/command/HistoryCommand.java
@@ -1,6 +1,11 @@
 package fr.pmk_bungee.command;
 
+import java.util.List;
+
+import fr.pmk_bungee.Main;
+import fr.pmk_bungee.utils.PlayerProfile;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class HistoryCommand extends Command {
@@ -13,7 +18,68 @@ public class HistoryCommand extends Command {
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		// TODO Auto-generated method stub
+		ProxyServer.getInstance().getScheduler().runAsync(Main.sharedInstance(), new Runnable() {
+			
+			@SuppressWarnings({ "deprecation", "unused" })
+			public void run() {
+				
+				if(sender.hasPermission("bungeestaff.command.history")) {
+					
+					if(args.length == 1) {
+						
+						PlayerProfile profile = new PlayerProfile(args[0]);
+						
+						if(profile != null) {
+							
+							String playername = args[0];
+							sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.commands.history", new String[] { "{PLAYERNAME}~" + playername }));
+							if(profile.isBanned()) {
+								
+				                List<String> msgs = Main.getConfigManager().getStringList("lang.commands.check.banned.positive", new String[] { "{NAME}~" + playername, "{REASON}~" +  profile.getBanReason(), "{BY}~" + profile.getBanBy(), "{REMAININGTIME}~" + profile.getRemainingbanTime() });
+				                for (String msg : msgs) {
+				                	sender.sendMessage(Main.PREFIX + msg);
+							
+				                }
+						
+							} else {
+								
+				                List<String> msgs = Main.getConfigManager().getStringList("lang.commands.check.banned.negative", new String[] { "{NAME}~" + playername });
+				                for (String msg : msgs) {
+				                	sender.sendMessage(Main.PREFIX + msg);
+				                  
+				                }
+							}
+							if(profile.isMuted()) {
+								
+				                List<String> msgs = Main.getConfigManager().getStringList("lang.commands.check.muted.positive", new String[] { "{NAME}~" + playername, "{REASON}~" + profile.getMuteReason(), "{BY}~" + profile.getMutedBy(), "{REMAININGTIME}~" + profile.getRemainingmuteTime() });
+				                for (String msg : msgs) {
+				                	sender.sendMessage(Main.PREFIX + msg);
+				                
+				                }
+							} else {
+								
+				                List<String> msgs = Main.getConfigManager().getStringList("lang.commands.check.muted.negative", new String[] { "{NAME}~" + playername });
+				                for (String msg : msgs) {
+				                	sender.sendMessage(Main.PREFIX + msg);
+				                }
+								
+							}
+							
+							
+				              
+			            }
+			            else
+			            {
+			              sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.errors.player_not_found"));
+			            }
 
-	}
-
-}
+					} else {
+			            sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.commands.check.syntax"));
+			          }
+			        } else {
+			          sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.errors.no_permissions"));
+			          }
+			      }
+			    });
+			  }
+			}
