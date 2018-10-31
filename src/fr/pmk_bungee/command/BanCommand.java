eef4347a0f6fdd0fe1e450f39f97bdaef9981677
Sufike <l.christ54260@gmail.com>
2018-10-31 11:35:23 +0100

    add ban command

--- src/fr/pmk_bungee/command/BanCommand.java
+++ src/fr/pmk_bungee/command/BanCommand.java
@@ -1,5 +1,6 @@
 package fr.pmk_bungee.command;
 
+import fr.pmk_bungee.Main;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -13,7 +14,22 @@ public class BanCommand extends Command {
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		if(sender.hasPermission("bungeestaff.command.ban")) {
+			if(args.length >= 4) {
+				
+				String playerName = args[0];
+				String reason = "";
+				for(int i = 3; i <= args.length - 1; i++) {
+					
+					reason = reason + args[i] + " ";
+					Main.getConfigManager().save();
+					Profile profile = new Profile(playerName)
+				
+				}
+			}
+			
+		}
 	}
 
 }
