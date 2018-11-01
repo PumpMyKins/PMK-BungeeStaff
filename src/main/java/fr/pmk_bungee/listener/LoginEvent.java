504affdcfb70aae95c836a19fd4a47f994e59075
Sufike <l.christ54260@gmail.com>
2018-11-01 01:32:18 +0100

    add logic

--- src/main/java/fr/pmk_bungee/listener/LoginEvent.java
+++ src/main/java/fr/pmk_bungee/listener/LoginEvent.java
@@ -1,5 +1,8 @@
 package fr.pmk_bungee.listener;
 
+import java.sql.ResultSet;
+
+import fr.pmk_bungee.Main;
 import fr.pmk_bungee.utils.PlayerProfile;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -10,30 +13,61 @@ public class LoginEvent implements Listener {
 	@EventHandler
 	public void onLogin(net.md_5.bungee.api.event.LoginEvent e) {
 		
-		PlayerProfile profile = new PlayerProfile(e.getConnection().getName());
-		if(profile.isBanned()) {
-			
-			long end = profile.getBanEnd();
-			long current = System.currentTimeMillis();
-			
-			if(end > 0L) {
+		try {
+		//Main.getMySQL().update("INSERT INTO MinecraftPlayer ");
+			PlayerProfile profile = new PlayerProfile(e.getConnection().getName());
+			if(profile.isBanned()) {
 				
-				if(end < current) {
-					profile.unban();
-				}
-			
-				else {
+				long end = profile.getBanEnd();
+				long current = System.currentTimeMillis();
+				
+				if(end > 0L) {
+					
+					if(end < current) {
+						profile.unban();
+					}
+				
+					else {
+					
+						e.setCancelled(true);
+						e.setCancelReason(profile.getBanKickMessage() + "");
+					}
+				}	else {
 				
 					e.setCancelled(true);
 					e.setCancelReason(profile.getBanKickMessage() + "");
-				}
-			}	else {
-			
-				e.setCancelled(true);
-				e.setCancelReason(profile.getBanKickMessage() + "");
-				}
+					}
+			}
 		}
+		catch (NullPointerException error) {
+			
+			Main.getMySQL().update("INSERT INTO MinecraftPlayer(username, uuid, ip) VALUES ('" + e.getConnection().getName() + "', '" + e.getConnection().getUniqueId() + "', '" + e.getConnection().getAddress() + "')");
+			PlayerProfile profile = new PlayerProfile(e.getConnection().getName());
 		
+			if(profile.isBanned()) {
+				
+				long end = profile.getBanEnd();
+				long current = System.currentTimeMillis();
+				
+				if(end > 0L) {
+					
+					if(end < current) {
+						profile.unban();
+					}
+				
+					else {
+					
+						e.setCancelled(true);
+						e.setCancelReason(profile.getBanKickMessage() + "");
+					}
+				}	else {
+				
+					e.setCancelled(true);
+					e.setCancelReason(profile.getBanKickMessage() + "");
+					}
+			}
+			
+		}
 		
 	}
 }
