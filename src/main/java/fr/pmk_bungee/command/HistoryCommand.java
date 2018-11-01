55690e322ff0c4ae245d691ab2678b88b5622e88
Sufike <l.christ54260@gmail.com>
2018-11-01 16:12:25 +0100

    debug

--- src/main/java/fr/pmk_bungee/command/HistoryCommand.java
+++ src/main/java/fr/pmk_bungee/command/HistoryCommand.java
@@ -19,11 +19,11 @@ public class HistoryCommand extends Command {
 			
 			if(sender.hasPermission("bungeestaff.command.history.personnal") && sender.getName().equals(args[0]) || sender.hasPermission("bungeestaff.command.history.player")) {
 				
-					/*PastPlayerProfile profile = new PastPlayerProfile(args[0]);
+					PastPlayerProfile profile = new PastPlayerProfile(args[0]);
 					if(profile != null) {
 						
 					sender.sendMessage(Main.PREFIX + Main.getConfigManager().getString("lang.command.history.succes", new String[] { "{NAME}~" + args[0] }));
-					
+					/*
 					if(profile.getPastBan().size() == 0) {
 						
 						sender.sendMessage(Main.getConfigManager().getString("lang.command.history.no_ban"));
@@ -45,8 +45,8 @@ public class HistoryCommand extends Command {
 							
 							sender.sendMessages(profile.getPastMuteMessage(i));
 						}
-					}
-				}*/
+					}*/
+				}
 			}
 		} else { System.out.println("MISSING ARGUMENT");}
 	}
