4104444e7fc1477aab42c97999454ccf23706dd1
Sufike <l.christ54260@gmail.com>
2018-11-01 17:04:31 +0100

    debug

--- src/main/java/fr/pmk_bungee/utils/PastPlayerProfile.java
+++ src/main/java/fr/pmk_bungee/utils/PastPlayerProfile.java
@@ -29,7 +29,7 @@ public class PastPlayerProfile {
 	
 	public void init() {
 		try {
-			
+			System.out.println(getUserID(playerName));
 			ResultSet rs = Main.getMySQL().getResult("SELECT * FROM PastBungeeBan WHERE userID ='" + getUserID(playerName) + "'");
 			while(rs.next()) {
 				
