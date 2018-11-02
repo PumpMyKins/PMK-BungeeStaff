0422a27fd47a882c95e56c961fe893bbaaa13e6a
Sufike <l.christ54260@gmail.com>
2018-11-02 02:47:26 +0100

    add PastMute bdd support

---
+++ src/main/java/fr/pmk_bungee/utils/PastMute.java
@@ -0,0 +1,37 @@
+package fr.pmk_bungee.utils;
+
+import java.sql.Date;
+
+public class PastMute {
+
+	private String playername;
+	private Date muteAt;
+	private int muteBy;
+	private String muteReason;
+	public String getPlayername() {
+		return playername;
+	}
+	public void setPlayername(String playername) {
+		this.playername = playername;
+	}
+	public Date getMuteAt() {
+		return muteAt;
+	}
+	public void setMuteAt(Date muteAt) {
+		this.muteAt = muteAt;
+	}
+	public int getMuteBy() {
+		return muteBy;
+	}
+	public void setMuteBy(int muteBy) {
+		this.muteBy = muteBy;
+	}
+	public String getMuteReason() {
+		return muteReason;
+	}
+	public void setMuteReason(String muteReason) {
+		this.muteReason = muteReason;
+	}
+	
+	
+}
