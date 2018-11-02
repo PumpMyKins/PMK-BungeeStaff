02dded896467f1778c83d9d599688673b6b0dd29
Sufike <l.christ54260@gmail.com>
2018-11-02 02:40:00 +0100

    add PastBan object and shower

---
+++ src/main/java/fr/pmk_bungee/utils/PastBan.java
@@ -0,0 +1,38 @@
+package fr.pmk_bungee.utils;
+
+import java.sql.Date;
+
+public class PastBan {
+
+	private String playername;
+	private Date banAt;
+	private int banBy;
+	private String banReason;
+	
+	public String getPlayername() {
+		return playername;
+	}
+	public void setPlayername(String playername) {
+		this.playername = playername;
+	}
+	public Date getBanAt() {
+		return banAt;
+	}
+	public void setBanAt(Date banAt) {
+		this.banAt = banAt;
+	}
+	public int getBanBy() {
+		return banBy;
+	}
+	public void setBanBy(int banBy) {
+		this.banBy = banBy;
+	}
+	public String getBanReason() {
+		return banReason;
+	}
+	public void setBanReason(String banReason) {
+		this.banReason = banReason;
+	}
+	
+	
+}
