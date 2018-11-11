b4e002fc005cd19e94049f73ff960762cbb53f95
Sufike <l.christ54260@gmail.com>
2018-11-11 12:24:09 +0100

    delete useless command

--- src/main/java/fr/pmk_bungee/utils/manager/CommandManager.java
+++ src/main/java/fr/pmk_bungee/utils/manager/CommandManager.java
@@ -2,7 +2,6 @@ package fr.pmk_bungee.utils.manager;
 
 import fr.pmk_bungee.Main;
 import fr.pmk_bungee.command.BanCommand;
-import fr.pmk_bungee.command.CheckPlayer;
 import fr.pmk_bungee.command.HistoryCommand;
 import fr.pmk_bungee.command.InformationCommand;
 import fr.pmk_bungee.command.KickCommand;
@@ -34,8 +33,6 @@ public class CommandManager {
 		pm.registerCommand(Main.sharedInstance(), new MuteCommand("mute"));
 		//BAN
 		pm.registerCommand(Main.sharedInstance(), new BanCommand("ban"));
-		//CHECK IF BAN / MUTE
-		pm.registerCommand(Main.sharedInstance(), new CheckPlayer("check"));
 		//GET HISTORY OF THE PLAYER
 		pm.registerCommand(Main.sharedInstance(), new HistoryCommand("history"));
 		// GET ALL INFORMATION OF A PLAYER
