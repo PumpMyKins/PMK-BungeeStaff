977373c9c8164bf1e48a00565a29d3241e6b589a
Sufike <l.christ54260@gmail.com>
2018-11-11 10:13:07 +0100

    fix login event

--- src/main/java/fr/pmk_bungee/listener/LoginEvent.java
+++ src/main/java/fr/pmk_bungee/listener/LoginEvent.java
@@ -1,11 +1,8 @@
 package fr.pmk_bungee.listener;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 
 import fr.pmk_bungee.Main;
-import fr.pmk_bungee.object.Player;
 import fr.pmk_bungee.utils.PlayerSituation;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -17,32 +14,26 @@ public class LoginEvent implements Listener {
 	public void onLogin(net.md_5.bungee.api.event.LoginEvent e) {
 
 		String playername = e.getConnection().getName();
-		try {
-			ResultSet rs = Main.getMySQL().getResult("SELECT * FROM MinecraftPlayer WHERE username = '"+e.getConnection().getName()+"'"); 
-			if(!rs.next()) {
-
-				Player player = new Player();
-				player.setFirstCome(new Timestamp(System.currentTimeMillis()));
-				player.setUsername(playername);
-				player.setUuid(e.getConnection().getUniqueId().toString());
-
-				/*Main.getMySQL().update("INSERT INTO MinecraftPlayer(username, uuid, ip, firstCome) VALUES ('" 
-						+  player.getUsername()
-						+ "', '" 
-						+ player.getUuid()
-						+ "', '" 
-						+ e.getConnection().getAddress().getAddress()
-						+ "', '" 
-						+ player.getFirstCome()
-						+ "')");
-				*/
+		PlayerSituation situation = new PlayerSituation(playername);
+		if(situation != null) {
+			if(situation.getPlayer().getUuid().isEmpty() || situation.getPlayer().getUsername().isEmpty() || situation.getPlayer().getFirstCome().toString().isEmpty()) {
+				
+				System.out.println("Player Not Exist in DB");
+				Timestamp now = new Timestamp(System.currentTimeMillis());
+				Main.getMySQL().update("INSERT INTO `MinecraftPlayer`(`username`, `uuid`, `ip`, `firstCome`) VALUES ("+e.getConnection().getName()+","+e.getConnection().getUniqueId()+","+e.getConnection().getAddress().getAddress()+","+now+" )");
+			
+			} else {
+				System.out.println("Already Exist ");
 			}
-		} catch (SQLException | NullPointerException error) {
-			error.printStackTrace();
-		}
+			if(PlayerSituation.isBanned(playername)){
+				
+				e.setCancelled(true);
+				e.setCancelReason(new TextComponent("GetBanned"));
+			}
 			
 			//TODO test ban
 		
 			//TODO kickBan
 		}
 	}
+}
