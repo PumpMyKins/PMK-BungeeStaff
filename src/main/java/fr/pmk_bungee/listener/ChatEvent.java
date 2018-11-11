9d785d54579cb2787ea2991f056aa038bc292b2f
Sufike <l.christ54260@gmail.com>
2018-11-11 11:04:08 +0100

    add Mute Support for Chat Event and exception for command

--- src/main/java/fr/pmk_bungee/listener/ChatEvent.java
+++ src/main/java/fr/pmk_bungee/listener/ChatEvent.java
@@ -1,6 +1,8 @@
 package fr.pmk_bungee.listener;
 
+import fr.pmk_bungee.Main;
 import fr.pmk_bungee.utils.PlayerSituation;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -10,20 +12,26 @@ public class ChatEvent implements Listener {
 	@EventHandler
 	public void onChar(net.md_5.bungee.api.event.ChatEvent e) {
 
-		ProxiedPlayer proxiedPlayer = (ProxiedPlayer) e.getSender();
-		PlayerSituation situation = new PlayerSituation(proxiedPlayer.getName());
-		
+		ProxiedPlayer p = (ProxiedPlayer) e.getSender();
+		PlayerSituation situation = new PlayerSituation(p.getName());
+
 		//TODO add test mute
 
-			if(e.getMessage().startsWith("/")) {
+		if(e.getMessage().startsWith("/")) {
 
-				return;
-			}
-			//TODO test mute 
+			return;
+		}
+		if (!situation.getMuteList().isEmpty()) {
+			if(PlayerSituation.isMuted(p.getName())) {
 				e.setCancelled(true);
-				//TODO send muteMEssage
+				for(String str : PlayerSituation.getMuteMessage(p.getName())) {
+
+					p.sendMessage(new TextComponent(Main.PREFIX + str));
+				}
 			}
-		//}
+
+		}				
 	}
-//}
+}
+
 
