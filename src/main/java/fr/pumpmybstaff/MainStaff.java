c0923ab1504999b5070c1b75c1d9d00b690f79a7
Clem-Fern <clemfern456@gmail.com>
2019-07-10 00:15:37 +0200

    Adding Main plugin class

---
+++ src/main/java/fr/pumpmybstaff/MainStaff.java
@@ -0,0 +1,22 @@
+package fr.pumpmybstaff;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class MainStaff extends Plugin{
+
+	public final static String PLUGIN_PREFIX = "§l§6[§2Pump§eMy§cBStaff§6]§r§f ";
+	
+	private MySql mySQL;
+	private ConfigManager configManager;
+
+	public MySql getMySQL() {return this.mySQL;}
+	public ConfigManager getConfigManager() {return this.configManager;}
+	
+	@Override
+	public void onEnable() {
+		
+		
+		
+	}
+	
+}
